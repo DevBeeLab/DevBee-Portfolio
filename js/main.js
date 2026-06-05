a9865a4f3a7abae9de77a9a182f55aae2f6daacd
Ibrahim Olawale <66759569+DevBeeLab@users.noreply.github.com>
2026-06-06 00:28:50 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -181,7 +181,7 @@ debugAnimations();
 // Typing animation
 const typedTextSpan = document.querySelector('.typed-text');
 const cursorSpan = document.querySelector('.cursor');
-const textArray = ["Ibrahim Olawale", "a Freelancer", " a Frontend Developer", "a UI Specialist", "a Problem Solver", "a Digital Craftsman"];
+const textArray = ["Ibrahim Olawale", " a Software Developer", "a Freelancer", "a UI Specialist", "a Problem Solver"];
 const typingDelay = 100;
 const erasingDelay = 50;
 const newTextDelay = 1500;
